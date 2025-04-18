6c4cd1b927f99a344aa101d7d34768fdda4ed004
Menam Khotabe <menamkhotabe@gmail.com>
2025-04-18 15:16:34 +0200

    first query

---
+++ sql_project/1_top_paying_jobs.sql
@@ -0,0 +1,20 @@
+select
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name as company_name
+from
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id  
+where
+    job_title_short like '%Machine Learning Engineer%' AND
+    job_location like '%Isreal%' AND
+    salary_year_avg is not null
+order by
+    salary_year_avg desc
+limit 10
+;
+
