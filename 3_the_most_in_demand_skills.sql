1dda32dfe64ca94840ab486a125573aee2195ebc
Menam Khotabe <menamkhotabe@gmail.com>
2025-04-18 21:40:07 +0200

    third query

---
+++ 3_the_most_in_demand_skills.sql
@@ -0,0 +1,13 @@
+select 
+    skills_dim.skills,
+    count(job_postings_fact.job_id) as demand_skills
+from job_postings_fact
+inner JOIN skills_job_dim on skills_job_dim.job_id = job_postings_fact.job_id
+INNER JOIN skills_dim on skills_dim.skill_id = skills_job_dim.skill_id
+WHERE 
+    job_title_short = 'Data Scientist' 
+GROUP BY
+    skills
+order by 
+    demand_skills desc
+limit 10;
