cf34231f369e5b0a97fc517945fa91538dca83c7
Menam Khotabe <menamkhotabe@gmail.com>
2025-04-18 20:45:22 +0200

    second query

--- sql_project/1_top_paying_jobs.sql
+++ sql_project/1_top_paying_jobs.sql
@@ -10,8 +10,8 @@ from
     job_postings_fact
 LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id  
 where
-    job_title_short like '%Machine Learning Engineer%' AND
-    job_location like '%Isreal%' AND
+    job_title_short like '%Data Analyst%' AND
+    job_location like '%Anywhere%' AND
     salary_year_avg is not null
 order by
     salary_year_avg desc
